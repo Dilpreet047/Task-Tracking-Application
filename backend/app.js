8c318178069292c5bf19348dbf2e991e9e1d6c98
Dilpreet047 <dilpreetluthra5@gmail.com>
2021-08-08 13:43:30 +0530

    Fixed the URI leak

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-
+require('dotenv/config')
 app.use(express.json())
 
 const proutes = require('./router/route');
 app.use('/data', proutes);
 
-const key = require('./keys/key').key;
-mongoose.connect(key, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+mongoose.connect(process.env.DB_connect, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log('Connected to DB')
 })
 
--- backend/keys/key.js
+++
@@ -1,5 +0,0 @@
-keys = {
-    key : "mongodb+srv://Dilpreet:dilpreet123@mern-stack.rshfn.mongodb.net/Task-Application?retryWrites=true&w=majority"
-}
-
-module.exports = keys;
